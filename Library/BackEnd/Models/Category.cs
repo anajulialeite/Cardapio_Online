8a63f30d0ee772750ff79a102012597b29bd7386
Paulo Mário Valente Bumba <143729787+PauloBumba@users.noreply.github.com>
2025-01-16 17:21:27 -0300

    rf

--- Library/BackEnd/Models/Category.cs
+++ Library/BackEnd/Models/Category.cs
@@ -6,6 +6,6 @@
 
         public string Name { get; set; } = string.Empty;
 
-        public string Item { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
     }
 }
