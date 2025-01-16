bbd903bf6f48ffa225d95ce30965fd283c812609
Ana Júlia Leite <leite.anajulia25@gmail.com>
2025-01-16 20:21:11 -0300

    Configurando o BD e ADD pacotes

--- Project/BackEnd/obj/Debug/net9.0/BackEnd.AssemblyInfo.cs
+++ Project/BackEnd/obj/Debug/net9.0/BackEnd.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a46de7c6a266b781e5c447fb449c14f1436632e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f3c559eb5a3bc3496a4e9202b521b9330941aea")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
