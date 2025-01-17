115ef724807c311f889e18e2bcd1cb296c94f6c5
Ana Júlia Leite <leite.anajulia25@gmail.com>
2025-01-16 22:38:08 -0300

    add relacionamentos

--- Project/BackEnd/obj/Debug/net9.0/BackEnd.AssemblyInfo.cs
+++ Project/BackEnd/obj/Debug/net9.0/BackEnd.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f3c559eb5a3bc3496a4e9202b521b9330941aea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fe816e3404620dc40b0fdb9f724c8b360fc89e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- back_end/cardapio_online/cardapio_online/obj/Debug/net8.0/cardapio_online.AssemblyInfo.cs
+++ back_end/cardapio_online/cardapio_online/obj/Debug/net8.0/cardapio_online.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cardapio_online")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd407fb3456a933515d8475ecbd5fb8234002f2e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fe816e3404620dc40b0fdb9f724c8b360fc89e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("cardapio_online")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cardapio_online")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
