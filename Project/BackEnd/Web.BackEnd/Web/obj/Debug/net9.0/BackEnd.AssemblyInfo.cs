2067d23bc5a469081b7e16b2d6246e48599e762f
Ana Júlia Leite <leite.anajulia25@gmail.com>
2025-02-07 12:24:47 -0300

    git pull

--- Project/BackEnd/Web.BackEnd/Web/obj/Debug/net9.0/BackEnd.AssemblyInfo.cs
+++ Project/BackEnd/Web.BackEnd/Web/obj/Debug/net9.0/BackEnd.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e49ac3a0e670cfaa6221decf3ac320d197a19aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d566dbf8be7f320a09658832a1acbbb99fa9a4d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
